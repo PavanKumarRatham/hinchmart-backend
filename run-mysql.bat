683fd0da80c088962e9c3afc106a19ef9d58a069
kottalarishi <kottalarishi4@gmail.com>
2026-08-20 12:07:50 +0530

    Configure local Aiven database

--- run-mysql.bat
+++ run-mysql.bat
@@ -1,9 +1,14 @@
 @echo off
-set "JAVA_HOME=C:\Program Files\Java\jdk-21"
-set "PATH=%JAVA_HOME%\bin;%PATH%"
+if not exist ".env" (
+    echo Missing .env. Copy .env.example to .env and set DB_PASSWORD.
+    exit /b 1
+)
+for /f "usebackq tokens=1,* delims==" %%A in (".env") do (
+    if not "%%A"=="" if not "%%A:~0,1"=="#" set "%%A=%%B"
+)
 echo ==========================================================
 echo Starting HinchMart Backend with MySQL Database...
-echo Make sure MySQL Server is running and password matches .env
+echo Using database settings from .env
 echo Swagger UI: http://localhost:8080/swagger-ui.html
 echo ==========================================================
-"C:\Program Files\JetBrains\IntelliJ IDEA 2026.2.0.1\plugins\maven-plugin\lib\maven3\bin\mvn.cmd" spring-boot:run
+mvn -Dmaven.repo.local="%USERPROFILE%\.m2\repository" spring-boot:run
